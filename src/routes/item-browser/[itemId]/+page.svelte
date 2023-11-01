702f8dc2e14f09acfb3b5b40b9e33f6e393fadc6
Jaiden DeChon <jdechon1995@gmail.com>
2023-11-01 00:31:17 +0000

    Styled header for individual item page

--- src/routes/item-browser/[itemId]/+page.svelte
+++ src/routes/item-browser/[itemId]/+page.svelte
@@ -6,7 +6,38 @@
     $: itemDetails = (data as any).itemDetails as GameItem;
 </script>
 
-<hgroup>
-    <h1>{ itemDetails.name }</h1>
-    <h2>{ itemDetails.examineText }</h2>
-</hgroup>
+<div class="individual-item-page__header">
+    <div class="individual-item-page__header-image-container">
+        <img
+            src="/item-images/{ itemDetails.image }"
+            alt={ itemDetails.name }
+        >
+    </div>
+    <hgroup>
+        <h1>{ itemDetails.name }</h1>
+        <h2>{ itemDetails.examineText }</h2>
+    </hgroup>
+</div>
+
+<style>
+    .individual-item-page__header {
+        display: flex;
+        gap: 1rem;
+    }
+
+    .individual-item-page__header-image-container {
+        height: 6rem;
+        width: 6rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: var(--card-sectionning-background-color);
+        border-radius: 50%;
+    }
+
+    .individual-item-page__header img {
+        width: 4.4rem;
+        height: 4.4rem;
+        object-fit: contain;
+    }
+</style>
