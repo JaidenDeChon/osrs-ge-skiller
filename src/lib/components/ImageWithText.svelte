1f23bbde59fb7849eafab5f3bc9634a70e457335
Jaiden DeChon <jaiden.dechon@gmail.com>
2023-11-02 21:49:47 -0500

    Created ImageWithText component

---
+++ src/lib/components/ImageWithText.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    // Image source attribute.
+    export let src: string;
+    // Image alt attribute.
+    export let alt: string;
+</script>
+
+<div class="image-with-text">
+    <img { src } { alt }>
+    <slot />
+</div>
+
+<style>
+    .image-with-text {
+        display: flex;
+        align-items: center;
+        gap: 1em;
+    }
+
+    .image-with-text img {
+        width: 1.6em;
+        object-fit: contain;
+    }
+
+    :global(.image-with-text > *) {
+        margin-bottom: 0;
+    }
+
+    :global(.image-with-text span) {
+        font-size: 0.75em;
+        color: var(--muted-color);
+    }
+</style>
