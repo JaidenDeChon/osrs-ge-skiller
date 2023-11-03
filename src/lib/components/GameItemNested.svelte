ba829ad460dfc4d523edb2f6e87b7cdd519c4f5e
Jaiden DeChon <jaiden.dechon@gmail.com>
2023-11-02 21:50:00 -0500

    Consumed ImageWithText component

--- src/lib/components/GameItemNested.svelte
+++ src/lib/components/GameItemNested.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { GameItem } from '$lib/models/GameItem';
     import { timeSince } from '$lib/helpers/timeSince';
+    import ImageWithText from './ImageWithText.svelte';
 
     export let gameItem: GameItem;
     export let isParent = false;
@@ -21,51 +22,57 @@
         {/if}
     </div>
 
-    <!-- GE prices -->
-    <div class="game-item-nested__stats">
-        <div class="icon-text-group">
-            <img src="/item-images/coins-few.png" alt="Just a few coins.">
-            <p>
-                { gameItem.lowPrice }
-                {#if gameItem.lowTime}
-                    <span>
-                        ({ timeSince(gameItem.lowTime, true) })
-                    </span>
-                {/if}
-            </p>
-        </div>
-    
-        <div class="icon-text-group">
-            <img src="/item-images/coins-lots.png" alt="Lots of coins!">
-            <p>
-                { gameItem.highPrice }
-                {#if gameItem.highTime}
-                    <span>
-                        ({ timeSince(gameItem.highTime, true) })
-                    </span>
-                {/if}
-            </p>
-        </div>
-    </div>
+    <div class="game-item-nested__values-container">
+        <!-- GE prices -->
+        <div class="game-item-nested__values">
+            <ImageWithText
+                src="/item-images/coins-few.png"
+                alt="Just a few coins."
+            >
+                <p>
+                    { gameItem.lowPrice }
+                    {#if gameItem.lowTime}
+                        <span>
+                            ({ timeSince(gameItem.lowTime, true) })
+                        </span>
+                    {/if}
+                </p>
+            </ImageWithText>
 
-    <!-- High/low alch (parent item only) -->
-    {#if isParent}
-        <div class="game-item-nested__stats">
-            <div class="icon-text-group">
-                <img src="/spell-images/low-level-alchemy.png" alt="Low level alchemy">
+            <ImageWithText
+                src="/item-images/coins-lots.png"
+                alt="Lots of coins!"
+            >
                 <p>
-                    { gameItem.highAlch }
+                    { gameItem.highPrice }
+                    {#if gameItem.highTime}
+                        <span>
+                            ({ timeSince(gameItem.highTime, true) })
+                        </span>
+                    {/if}
                 </p>
-            </div>
-        
-            <div class="icon-text-group">
-                <img src="/spell-images/high-level-alchemy.png" alt="High level alchemy">
-                <p>
-                    { gameItem.lowAlch }
-                </p>
-            </div>
+            </ImageWithText>
         </div>
-    {/if}
+
+        <!-- High/low alch (parent item only) -->
+        {#if isParent}
+            <div class="game-item-nested__values">
+                <ImageWithText
+                    src="/spell-images/low-level-alchemy.png"
+                    alt="Low level alchemy"
+                >
+                    <p>{ gameItem.highAlch }</p>
+                </ImageWithText>
+
+                <ImageWithText
+                    src="/spell-images/high-level-alchemy.png"
+                    alt="High level alchemy"
+                >
+                    <p>{ gameItem.lowAlch }</p>
+                </ImageWithText>
+            </div>
+        {/if}
+    </div>
 
     <!-- Checkbox for including in total (!isParent only) -->
     {#if !isParent}
@@ -119,18 +126,16 @@
         margin-bottom: 0;
     }
 
-    .game-item-nested__stats {
-        margin-bottom: 1em;
+    .game-item-nested__values-container {
         display: flex;
-        justify-content: space-around;
+        gap: 3em;
+        /* justify-content: space-evenly; */
     }
 
-    .icon-text-group {
-        min-width: 40%;
-    }
-
-    .icon-text-group span {
-        font-size: 0.75em;
+    .game-item-nested__values {
+        margin-bottom: 1em;
+        display: inline;
+        flex-direction: column;
     }
 
     .game-item-nested__ingredients {
@@ -146,10 +151,14 @@
         color: var(--muted-color);
     }
 
+    :global(.image-with-text) {
+        flex: 1;
+    }
+
     @media (prefers-color-scheme: dark) {
         .game-item-nested {
-            border-width: 0.2em !important;
-            border-color: var(--card-sectionning-background-color);
+            background-color: rgba(255, 255, 255, 0.03);
+            border-color: transparent;
         }
     }
 
