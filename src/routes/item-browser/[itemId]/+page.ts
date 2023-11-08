c5ac82aaaaa7513d4dc9192b6ef1c995b65a1a23
Jaiden DeChon <jaiden.dechon@gmail.com>
2023-11-08 17:25:11 -0600

    Small adjustment to load function

--- src/routes/item-browser/[itemId]/+page.ts
+++ src/routes/item-browser/[itemId]/+page.ts
@@ -3,10 +3,9 @@ import type { GameItem } from '$lib/models/GameItem';
 
 export const load: PageLoad = async ({ params, fetch }) => {
     const itemId = params.itemId;
-    let itemDetails: GameItem;
 
     const searchParams = new URLSearchParams({ itemId });
     const response = await fetch('/api/game-item-by-id' + '?' + searchParams.toString());
-    itemDetails = await response.json();
+    const itemDetails: GameItem = await response.json();
     return { itemDetails };
 }
