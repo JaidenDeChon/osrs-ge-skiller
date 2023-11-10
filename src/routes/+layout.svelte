52a94fe0befa52938e89354d9e5c8946089f75a6
Jaiden DeChon <jdechon1995@gmail.com>
2023-11-10 06:44:10 +0000

    Added .netlify to gitignore

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
     <nav>
         <ul>
             <li>
-                <strong>🚩 osrs-ge-skiller</strong>
+                <strong>osrs-ge-skiller</strong>
             </li>
         </ul>
     
