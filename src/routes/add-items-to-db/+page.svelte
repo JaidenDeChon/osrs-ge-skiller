945ed86f3fca1489375c5835412b34db70ca4abe
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-05-29 09:44:36 -0500

    Temporarily opening up DB insertion page in prod

--- src/routes/add-items-to-db/+page.svelte
+++ src/routes/add-items-to-db/+page.svelte
@@ -198,251 +198,253 @@
 </script>
 
 <div class="p-6">
-    {#if !isDev}
+    <!-- {#if !isDev}
         <p class="mb-4">Hey! You're not supposed to be here!</p>
         <a href="/">Go back home</a>
-    {/if}
+    {/if} -->
 
-    {#if isDev}
-        <h2 class="h2">Add item to DB</h2>
+    <!-- {#if isDev}
+        
+    {/if} -->
 
-        <!-- Category and skill parents -->
-        <h3 class="h3 mt-6 mb-4">Category and skill parents</h3>
+    <h2 class="h2">Add item to DB</h2>
 
-        <label
-            class="flex flex-col mb-4"
-            for="category"
+    <!-- Category and skill parents -->
+    <h3 class="h3 mt-6 mb-4">Category and skill parents</h3>
+
+    <label
+        class="flex flex-col mb-4"
+        for="category"
+    >
+        Category
+        <input
+            class="input variant-ghost-primary"
+            type="text"
+            name="category"
+            bind:value={category}
+        />
+    </label>
+
+    <label
+        class="flex flex-col mb-4"
+        for="skill"
+    >
+        Skill
+        <input
+            class="input variant-ghost-primary"
+            type="text"
+            name="skill"
+            bind:value={skill}
+        />
+    </label>
+
+    <p>Or select from existing:</p>
+    <div class="flex gap-4">
+        <select
+            placeholder="category"
+            class="select variant-ghost-primary"
+            name="category"
+            bind:value={category}
         >
-            Category
-            <input
-                class="input variant-ghost-primary"
-                type="text"
-                name="category"
-                bind:value={category}
-            />
-        </label>
+            {#each allCategories as categoryName}
+                <option>{ categoryName }</option>
+            {/each}
+        </select> 
 
-        <label
-            class="flex flex-col mb-4"
-            for="skill"
+        <select
+            placeholder="skill"
+            class="select variant-ghost-primary"
+            name="skill"
+            bind:value={skill}
         >
-            Skill
-            <input
-                class="input variant-ghost-primary"
-                type="text"
-                name="skill"
-                bind:value={skill}
-            />
-        </label>
+            {#each allSkills as skill}
+                <option>{ skill }</option>
+            {/each}
+        </select>
+    </div>
 
-        <p>Or select from existing:</p>
-        <div class="flex gap-4">
-            <select
-                placeholder="category"
-                class="select variant-ghost-primary"
-                name="category"
-                bind:value={category}
-            >
-                {#each allCategories as categoryName}
-                    <option>{ categoryName }</option>
-                {/each}
-            </select> 
+    <!-- General information -->
+    <h3 class="h3 mt-6 mb-4">General information</h3>
 
+    <label
+        class="flex flex-col mb-4"
+        for="id"
+    >
+        Id
+        <input
+            class="input variant-ghost-primary"
+            type="text"
+            name="id"
+            bind:value={id}
+        />
+    </label>
+
+    <label
+        class="flex flex-col mb-4"
+        for="name"
+    >
+        Name
+        <input
+            class="input variant-ghost-primary"
+            type="text"
+            name="name"
+            bind:value={name}
+        />
+    </label>
+
+    <label
+        class="flex flex-col mb-4"
+        for="examineText"
+    >
+        Examine text
+        <input
+            class="input variant-ghost-primary"
+            type="text"
+            name="examineText"
+            bind:value={examineText}
+        />
+    </label>
+
+    <!-- Alchemy values -->
+    <h3 class="h3 mt-6 mb-4">Alchemy values</h3>
+
+    <label
+        class="flex flex-col mb-4"
+        for="highAlch"
+    >
+        High alch
+        <input
+            class="input variant-ghost-primary"
+            type="number"
+            name="highAlch"
+            bind:value={highAlch}
+        />
+    </label>
+
+    <label
+        class="flex flex-col mb-4"
+        for="lowAlch"
+    >
+        Low alch
+        <input
+            class="input variant-ghost-primary"
+            type="number"
+            name="lowAlch"
+            bind:value={lowAlch}
+        />
+    </label>
+
+    <!-- Experience granted -->
+    <h3 class="h3 mt-6 mb-4">Experience Granted</h3>
+
+    <button
+        class="btn variant-filled-primary mb-4"
+        on:click={addExperienceGranted}
+    >
+        add
+    </button>
+
+    {#each creationSpecs.experienceGranted as xpGrantedEntry}
+        <label class="flex flex-col mb-4">
+            Skill name
             <select
-                placeholder="skill"
                 class="select variant-ghost-primary"
-                name="skill"
-                bind:value={skill}
+                bind:value={xpGrantedEntry.skillName}
             >
-                {#each allSkills as skill}
+                {#each Object.values(InGameSkillNamesEnum) as skill}
                     <option>{ skill }</option>
                 {/each}
             </select>
-        </div>
-
-        <!-- General information -->
-        <h3 class="h3 mt-6 mb-4">General information</h3>
-
-        <label
-            class="flex flex-col mb-4"
-            for="id"
-        >
-            Id
-            <input
-                class="input variant-ghost-primary"
-                type="text"
-                name="id"
-                bind:value={id}
-            />
         </label>
 
-        <label
-            class="flex flex-col mb-4"
-            for="name"
-        >
-            Name
-            <input
-                class="input variant-ghost-primary"
-                type="text"
-                name="name"
-                bind:value={name}
-            />
-        </label>
-
-        <label
-            class="flex flex-col mb-4"
-            for="examineText"
-        >
-            Examine text
-            <input
-                class="input variant-ghost-primary"
-                type="text"
-                name="examineText"
-                bind:value={examineText}
-            />
-        </label>
-
-        <!-- Alchemy values -->
-        <h3 class="h3 mt-6 mb-4">Alchemy values</h3>
-
-        <label
-            class="flex flex-col mb-4"
-            for="highAlch"
-        >
-            High alch
+        <label class="flex flex-col mb-4">
+            Amount of xp granted
             <input
                 class="input variant-ghost-primary"
                 type="number"
-                name="highAlch"
-                bind:value={highAlch}
+                bind:value={xpGrantedEntry.experienceAmount}
             />
         </label>
+    {/each}
 
-        <label
-            class="flex flex-col mb-4"
-            for="lowAlch"
-        >
-            Low alch
+    <!-- Required Skills -->
+    <h3 class="h3 mt-6 mb-4">Required Skills</h3>
+
+    <button
+        class="btn variant-filled-primary mb-4"
+        on:click={addRequiredSkill}
+    >
+        add
+    </button>
+
+    {#each creationSpecs.requiredSkills as requiredSkill}
+        <label class="flex flex-col mb-4">
+            Skill name
+            <select
+                class="select variant-ghost-primary"
+                bind:value={requiredSkill.skillName}
+            >
+                {#each Object.values(InGameSkillNamesEnum) as skill}
+                    <option>{ skill }</option>
+                {/each}
+            </select>
+        </label>
+
+        <label class="flex flex-col mb-4">
+            Skill level
             <input
                 class="input variant-ghost-primary"
                 type="number"
-                name="lowAlch"
-                bind:value={lowAlch}
+                bind:value={requiredSkill.skillLevel}
+            />
+        </label>
+    {/each}
+
+    <!-- Ingredients -->
+    <h3 class="h3 mt-6 mb-4">Ingredients</h3>
+
+    <button
+        class="btn variant-filled-primary mb-4"
+        on:click={addIngredient}
+    >
+        add
+    </button>
+
+    {#each creationSpecs.ingredients as ingredient}
+        <label class="flex flex-col mb-4">
+            Game Item
+            <select bind:value={ingredient.item} class="select variant-ghost-primary">
+                {#each allGameItems as gameItem}
+                    <option value={gameItem._id}>{ gameItem.name }</option>
+                {/each}
+            </select>
+        </label>
+
+        <label class="flex flex-col mb-4">
+            Amount
+            <input
+                class="input variant-ghost-primary"
+                type="number"
+                bind:value={ingredient.amount}
             />
         </label>
 
-        <!-- Experience granted -->
-        <h3 class="h3 mt-6 mb-4">Experience Granted</h3>
+        <label class="flex flex-col mb-4">
+            Consumed during creation?
+            <input
+                class="input variant-ghost-primary"
+                type="checkbox"
+                bind:checked={ingredient.consumedDuringCreation}
+            />
+        </label>
+    {/each}
 
-        <button
-            class="btn variant-filled-primary mb-4"
-            on:click={addExperienceGranted}
-        >
-            add
-        </button>
+    <br>
 
-        {#each creationSpecs.experienceGranted as xpGrantedEntry}
-            <label class="flex flex-col mb-4">
-                Skill name
-                <select
-                    class="select variant-ghost-primary"
-                    bind:value={xpGrantedEntry.skillName}
-                >
-                    {#each Object.values(InGameSkillNamesEnum) as skill}
-                        <option>{ skill }</option>
-                    {/each}
-                </select>
-            </label>
-
-            <label class="flex flex-col mb-4">
-                Amount of xp granted
-                <input
-                    class="input variant-ghost-primary"
-                    type="number"
-                    bind:value={xpGrantedEntry.experienceAmount}
-                />
-            </label>
-        {/each}
-
-        <!-- Required Skills -->
-        <h3 class="h3 mt-6 mb-4">Required Skills</h3>
-
-        <button
-            class="btn variant-filled-primary mb-4"
-            on:click={addRequiredSkill}
-        >
-            add
-        </button>
-
-        {#each creationSpecs.requiredSkills as requiredSkill}
-            <label class="flex flex-col mb-4">
-                Skill name
-                <select
-                    class="select variant-ghost-primary"
-                    bind:value={requiredSkill.skillName}
-                >
-                    {#each Object.values(InGameSkillNamesEnum) as skill}
-                        <option>{ skill }</option>
-                    {/each}
-                </select>
-            </label>
-
-            <label class="flex flex-col mb-4">
-                Skill level
-                <input
-                    class="input variant-ghost-primary"
-                    type="number"
-                    bind:value={requiredSkill.skillLevel}
-                />
-            </label>
-        {/each}
-
-        <!-- Ingredients -->
-        <h3 class="h3 mt-6 mb-4">Ingredients</h3>
-
-        <button
-            class="btn variant-filled-primary mb-4"
-            on:click={addIngredient}
-        >
-            add
-        </button>
-
-        {#each creationSpecs.ingredients as ingredient}
-            <label class="flex flex-col mb-4">
-                Game Item
-                <select bind:value={ingredient.item} class="select variant-ghost-primary">
-                    {#each allGameItems as gameItem}
-                        <option value={gameItem._id}>{ gameItem.name }</option>
-                    {/each}
-                </select>
-            </label>
-    
-            <label class="flex flex-col mb-4">
-                Amount
-                <input
-                    class="input variant-ghost-primary"
-                    type="number"
-                    bind:value={ingredient.amount}
-                />
-            </label>
-
-            <label class="flex flex-col mb-4">
-                Consumed during creation?
-                <input
-                    class="input variant-ghost-primary"
-                    type="checkbox"
-                    bind:checked={ingredient.consumedDuringCreation}
-                />
-            </label>
-        {/each}
-
-        <br>
-
-        <button
-            class="btn variant-filled-primary mt-6"
-            on:click={submitNewItem}
-        >
-            Submit
-        </button>
-    {/if}
+    <button
+        class="btn variant-filled-primary mt-6"
+        on:click={submitNewItem}
+    >
+        Submit
+    </button>
 </div>
