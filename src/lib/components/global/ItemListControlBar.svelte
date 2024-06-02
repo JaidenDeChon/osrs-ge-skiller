aa7003f06f86ceb480dd39a2cf14c858bfc88b08
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-06-01 19:43:55 -0500

    Removed viewMode from ItemListControlBar component

--- src/lib/components/global/ItemListControlBar.svelte
+++ src/lib/components/global/ItemListControlBar.svelte
@@ -1,16 +1,12 @@
 <script lang="ts">
     import { get } from 'svelte/store';
-    import { createEventDispatcher } from 'svelte';
-    import { SlideToggle, RadioGroup, RadioItem, type PopupSettings, popup } from '@skeletonlabs/skeleton';
+    import { SlideToggle, type PopupSettings, popup } from '@skeletonlabs/skeleton';
     import { getModalStore } from '@skeletonlabs/skeleton';
 	import { ModalNamesEnum } from '$lib/enums/ModalNamesEnum';
 	import { InGameSkillNamesEnum } from '$lib/enums/InGameSkillNamesEnum';
     import { filterItemsStore } from '$lib/stores/filterItemBrowserByPlayerLevelsStore';
 
     const modalStore = getModalStore();
-    const dispatch = createEventDispatcher();
-
-    export let viewMode: 0 | 1;
 
     const popupClick: PopupSettings = {
         event: 'click',
@@ -96,37 +92,6 @@
             <option value="ascending">Lowest first</option>
         </select>
     </label>
-
-    <div class="flex flex-col gap-1 mr-auto lg:mr-0 lg:ml-auto">
-        <span class="span">View</span>
-        <div>
-            <RadioGroup
-                display="flex"
-                background="bg-primary-100 dark:bg-primary-900"
-                border="border border-3 border-primary-500"
-                hover="hover:variant-glass-primary"
-                active="variant-filled-primary"
-            >
-                <RadioItem
-                    bind:group={viewMode}
-                    name="justify"
-                    value={0}
-                    on:change={(value) => dispatch('layout-change', 0)}
-                >
-                    <i class="fa-solid fa-table-cells-large"></i>
-                </RadioItem>
-                
-                <RadioItem
-                    bind:group={viewMode}
-                    name="justify"
-                    value={1}
-                    on:change={(value) => dispatch('layout-change', 1)}
-                >
-                    <i class="fa-solid fa-bars"></i>
-                </RadioItem>
-            </RadioGroup>
-        </div>
-    </div>
 </div>
 
 <!-- Popup element for "Filter by my skill levels" help button -->
